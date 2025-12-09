e2376186bfc140ced914594eaaeef33530780f4f
Marc <158296221+Axo34080@users.noreply.github.com>
2025-12-09 13:51:04 +0100

    Fix formatting and add missing newline at end of file

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -125,8 +125,9 @@ export default function About() {
                   {item.status && (
                     <div className={`absolute left-4 md:left-1/2 transform md:-translate-x-1/2 w-3 h-3 bg-${item.color} rounded-full border-2 border-cyber-dark ring-4 ring-cyber-magenta/30 animate-pulse`}></div>
                   )}
+                </div>
+              ))}
             </div>
-          ))}
           </div>
         </div>
       </section>
