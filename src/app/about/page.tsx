7990156e98640205ee2455a752d001dd6252b262
Marc <158296221+Axo34080@users.noreply.github.com>
2025-12-09 13:46:31 +0100

    Fix JSX syntax in about page component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -126,6 +126,7 @@ export default function About() {
                     <div className={`absolute left-4 md:left-1/2 transform md:-translate-x-1/2 w-3 h-3 bg-${item.color} rounded-full border-2 border-cyber-dark ring-4 ring-cyber-magenta/30 animate-pulse`}></div>
                   )}
             </div>
+          ))}
           </div>
         </div>
       </section>
